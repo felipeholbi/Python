602bd54475c29c1515e109406cbffe2f94949147
felipeholbi <4758@holbertonstudents.com>
2022-10-25 21:17:46 -0500

    stdin and stdout input string

---
+++ stdin_stdout.py
@@ -0,0 +1,8 @@
+# THe first line contait the input line in the format stdin
+inputString = input()
+
+# This line print in stdout something
+print('Hello World')
+
+# The last line print in stdout the input_string
+print(inputString)
